a0651e99749f8c9fa22f4d80e49d4f7ad679d66c
saksham-sqelabs <saksham.sharma@sqelabs.com>
2024-04-12 12:53:45 +0530

    changes

--- src/main/java/test/HelloWorldPrint.java
+++ src/main/java/test/HelloWorldPrint.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class HelloWorldPrint {
     @Test
     public void hello_world(){
+        if(0!=0)
         System.out.println("Hello World!");
     }
 }
