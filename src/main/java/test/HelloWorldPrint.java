c20c653a230225c181c492d04dc28dc3b1de666e
saksham-sqelabs <saksham.sharma@sqelabs.com>
2024-04-12 12:46:42 +0530

    sample code for jenkins

---
+++ src/main/java/test/HelloWorldPrint.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldPrint {
+    @Test
+    public void hello_world(){
+        System.out.println("Hello World!");
+    }
+}
