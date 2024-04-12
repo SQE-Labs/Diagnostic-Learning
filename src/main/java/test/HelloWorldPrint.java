b6775c86ca6f2c3f0562065f1a050f58ad5f4271
saksham-sqelabs <saksham.sharma@sqelabs.com>
2024-04-12 12:56:01 +0530

    wrong code

--- src/main/java/test/HelloWorldPrint.java
+++ src/main/java/test/HelloWorldPrint.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 public class HelloWorldPrint {
     @Test
     public void hello_world(){
-        if(0!=0)
+        @Asert.assertTrue(true);
+        System.out.println("Whatsapp is working fine");
         System.out.println("Hello World!");
     }
 }
